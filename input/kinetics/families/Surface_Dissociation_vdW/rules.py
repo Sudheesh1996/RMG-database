c9d71f3549158014fcee2687082f0307d59505b8
davidfarinajr <farina.d@northeastern.edu>
2021-07-02 19:16:42 -0400

    Surface_Dissociation_vdW rule change

--- input/kinetics/families/Surface_Dissociation_vdW/rules.py
+++ input/kinetics/families/Surface_Dissociation_vdW/rules.py
@@ -10,21 +10,16 @@ entry(
     index = 1,
     label = "Combined;VacantSite",
     kinetics = SurfaceArrheniusBEP(
-        A = (4.879e15, 'm^2/(mol*s)'),
+        A = (1.0e17, 'm^2/(mol*s)'),
         n = 0,
-        alpha = 0.6,
-        E0 = (10.8384576, 'kcal/mol'),
+        alpha = 0.4,
+        E0 = (0.8, 'eV/molecule'),
         Tmin = (200, 'K'),
         Tmax = (3000, 'K'),
     ),
     rank = 0,
     shortDesc = u"""Default""",
     longDesc = u"""
-Reaction 30 from table 2 in "Mechanism of Methanol Synthesis on Cu through CO2
-and CO Hydrogenation", Grabow and Mavrikakis.  doi:10.1021/cs200055d
-E0 is Ea
 
-A factor from paper / surface site density of Cu
-1.436e11 m^4/(mol^2 * s) / 2.943e‐5 mol/m^2 = 4.879e15 m^2/(mol*s)
 """
 )
